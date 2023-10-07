441aac7b85d363d84b44eb2545b6ca202044d793
sebcod <sebastien.codus@live.fr>
2023-10-07 11:04:05 +0200

    feat(script):add-infinit-slide

--- assets/script.js
+++ assets/script.js
@@ -19,49 +19,40 @@ const slides = [
 ];
 
 const banner_img = document.querySelector(".banner-img");
+const banner_tagLine = document.querySelector("#banner p");
 const arrow_left = document.querySelector(".arrow_left");
 const arrow_right = document.querySelector(".arrow_right");
 const dots_container = document.querySelector(".dots");
 
-// init slide
-slides.forEach((slide) => {
+function update_slider() {
+  dots[slide_index].classList.add("dot_selected");
+  banner_img.src = `./assets/images/slideshow/${slides[slide_index].image}`;
+  banner_tagLine.innerHTML = slides[slide_index].tagLine;
+}
+
+// create dots
+slides.forEach(() => {
   const dot = document.createElement("div");
   dot.classList.add("dot");
   dots_container.appendChild(dot);
-  slides.indexOf(slide) === 0 ? dot.classList.add("dot_selected") : "";
 });
 
 const dots = document.querySelectorAll(".dot");
-const dots_array = Array.from(dots);
-const banner_tagLine = document.getElementById("banner").children[3];
-let current_slide_index = 0;
+
+let slide_index = 0;
+
+update_slider();
 
 // click left
 arrow_left.addEventListener("click", () => {
-  get_current_slide();
-  set_next_slide("next_slide_left");
+  dots[slide_index].classList.remove("dot_selected");
+  slide_index - 1 < 0 ? (slide_index = slides.length - 1) : slide_index--;
+  update_slider();
 });
 
 //click right
 arrow_right.addEventListener("click", () => {
-  get_current_slide();
-  set_next_slide("next_slide_right");
+  dots[slide_index].classList.remove("dot_selected");
+  slide_index + 1 > slides.length - 1 ? (slide_index = 0) : slide_index++;
+  update_slider();
 });
-
-function get_current_slide() {
-  dots_array.forEach((dot) => {
-    if (dot.classList.contains("dot_selected")) {
-      current_slide_index = dots_array.indexOf(dot);
-      dots_array[current_slide_index].classList.remove("dot_selected");
-    }
-  });
-}
-
-function set_next_slide(arrow) {
-  arrow === "next_slide_left"
-    ? (current_slide_index -= 1)
-    : (current_slide_index += 1);
-  dots_array[current_slide_index].classList.add("dot_selected");
-  banner_img.src = `./assets/images/slideshow/${slides[current_slide_index].image}`;
-  banner_tagLine.innerHTML = slides[current_slide_index].tagLine;
-}
